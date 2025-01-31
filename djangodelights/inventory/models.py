499fa0607c2771854e4d3bb07a4f4c61fa8bf4bd
Carli Kerr <carlimkerr@gmail.com>
2025-01-31 11:40:52 -0600

    Added model for Ingredients

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=30)
+    quantity = models.IntegerField(default=0)
+    unit = models.CharField(max_length = 10)
+    unit_price = models.IntegerField(default=0)
+
+class MenuItems(models.Model):
+    pass
+
+class RecipeRequirements(models.Model):
+    pass
+
+class Purchases(models.Model):
+    pass
